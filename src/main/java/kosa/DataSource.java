9162454cf9c6d0bac7098684c108a4a71f7a16d9
kosarep <grzesiek.kosicki@gmail.com>
2017-06-25 21:32:55 +0200

    z domu baza zrobiona

--- src/main/java/kosa/DataSource.java
+++ src/main/java/kosa/DataSource.java
@@ -1,44 +1,46 @@
 package kosa;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by JF194405 on 23.06.2017.
  */
 public class DataSource {
-    private HashMap<String,String> userSource;
+    //private HashMap<String,String> userSource;
     private QueryExecutor queryExecutor;
 
     public DataSource() {
-        userSource = new HashMap<String, String>();
-        generateTestData();
+        //userSource = new HashMap<String, String>();
+        queryExecutor = new QueryExecutor();
     }
-
+    /*
     public void register(String name, String password) {
         userSource.put(name, password);
     }
-
+*/
     public boolean userExists(User user) {
-        String passwordFromSource = userSource.get(user.getName());
-        if (passwordFromSource != null) {
-            return user.getPassword().equals(passwordFromSource);
+        //String passwordFromSource = userSource.get(user.getName());
+        List<String> passwordFromSource = queryExecutor.doSelectQuery("SELECT * FROM login_server.users WHERE name = '" + user.getName() + "';", "password");
+        if (passwordFromSource.size() != 0) {
+            return user.getPassword().equals(passwordFromSource.get(0));
         } else {
             return false;
         }
     }
-
+/*
     private void generateTestData() {
         userSource.put("admin", "test");
         userSource.put("user", "passUser");
-        queryExecutor = new QueryExecutor();
-        queryExecutor.doQuery("INSERT INTO login_server.users VALUES (NULL, 'elo', 'admin');");
     }
-
+*/
     public boolean nameExists(User user) {
-        return userSource.containsKey(user.getName());
+        //return userSource.containsKey(user.getName());
+        return queryExecutor.isPresent("SELECT name FROM login_server.users WHERE name = '" + user.getName() + "';");
     }
 
     public void createUser(User user) {
-        userSource.put(user.getName(), user.getPassword());
+        //userSource.put(user.getName(), user.getPassword());
+        queryExecutor.doInsertQuery("INSERT INTO login_server.users VALUES (NULL, '" + user.getName() + "', '" + user.getPassword() + "');");
     }
 }
--- src/main/java/kosa/QueryExecutor.java
+++ src/main/java/kosa/QueryExecutor.java
@@ -1,9 +1,8 @@
 package kosa;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by JF194405 on 23.06.2017.
@@ -12,12 +11,12 @@ public class QueryExecutor {
     private final static String DBURL = "jdbc:mysql://127.0.0.1:3306/login_server";
     private final static String DBUSER = "root";
     private final static String DBPASS = "admin";
-    private final static String DBDRIVER = "com.mysql.jdbc.Driver";
+    private final static String DBDRIVER = "com.mysql.cj.jdbc.Driver";
 
     private Connection connection;
     private Statement statement;
 
-    public void doQuery(String query) {
+    public void doInsertQuery(String query) {
         try {
             Class.forName(DBDRIVER).newInstance();
             connection = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
@@ -30,4 +29,36 @@ public class QueryExecutor {
         }
     }
 
+    public boolean isPresent(String query) {
+        boolean isOrNo = true;
+        try {
+            Class.forName(DBDRIVER).newInstance();
+            connection = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
+            statement = connection.createStatement();
+            isOrNo = statement.executeQuery(query).first();
+            statement.close();
+            connection.close();
+        } catch(InstantiationException | IllegalAccessException |ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return isOrNo;
+    }
+
+    public List<String> doSelectQuery(String query, String columnName) {
+        List<String> results = new ArrayList<>();
+        try {
+            Class.forName(DBDRIVER).newInstance();
+            connection = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                results.add(resultSet.getString(columnName));
+            }
+            statement.close();
+            connection.close();
+        } catch(InstantiationException | IllegalAccessException |ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return results;
+    }
 }
